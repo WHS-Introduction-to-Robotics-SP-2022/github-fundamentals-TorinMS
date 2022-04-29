3242455221c35c89de89ee1486ff63241b49fabf
TorinMS <99278941+TorinMS@users.noreply.github.com>
2022-04-29 09:37:32 -0600

    Create problem2.ino

---
+++ problem2.ino
@@ -0,0 +1,16 @@
+
+
+void setup() {
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(11,OUTPUT);
+  analogWrite(9,128);
+  analogWrite(10,2);
+  analogWrite(11,16);
+  
+  
+}
+
+void loop() {
+    
+}
